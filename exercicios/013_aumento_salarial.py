75b18a321a5a3a6d2dec3fdbf8e8098b655e8f55
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-03 23:59:35 -0300

    Create 013_aumento_salarial.py

---
+++ exercicios/013_aumento_salarial.py
@@ -0,0 +1,5 @@
+salario=float(input('\nOla colaborador\nQual o seu salario? '))
+
+aumento=salario+(15*salario/100)
+
+print(f'Seu salario era {salario:.2f}\nCom um aumento de 15%\nSeu salario vai para {aumento}')
