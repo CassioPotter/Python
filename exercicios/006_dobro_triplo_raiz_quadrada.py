a58a28983a0f1ca4ccb18df6a562b0ec66f02232
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-03 22:22:09 -0300

    Create 006_dobro_triplo_raiz_quadrada.py

---
+++ exercicios/006_dobro_triplo_raiz_quadrada.py
@@ -0,0 +1,10 @@
+valor=int(input('\ndigite um valor para saber seu dobro, seu triplo e sua raiz quadrada \n'))
+
+dobro=valor*2
+triplo=valor*3
+raiz=valor**(1/2)
+
+print(f'\nVoce digitou {valor},')
+print(f'Seu dobro é {dobro},')
+print(f'Seu triplo é {triplo}')
+print(f'Sua raiz quadrada é {raiz}')
