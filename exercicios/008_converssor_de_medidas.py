12da706b31a7d2ff74c5669dde1d31a19343df62
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-03 22:50:58 -0300

    Create 008_converssor_de_medidas.py

---
+++ exercicios/008_converssor_de_medidas.py
@@ -0,0 +1,8 @@
+metros=float(input('\nDigite a medida em metros '))
+
+centimetros=metros*100
+milimetros=metros*1000
+
+print(f'\nVoce digitou {metros}m')
+print(f'{metros}m em centimetros da {centimetros}cm')
+print(f'{metros}m em milimetros da {milimetros}mm')
