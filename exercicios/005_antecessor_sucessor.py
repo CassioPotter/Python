95a11d3d78ab4f717ee47f1677c84da937635e66
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-03 22:12:02 -0300

    Create 005_antecessor_sucessor.py

---
+++ exercicios/005_antecessor_sucessor.py
@@ -0,0 +1,4 @@
+valor=int(input('Digite um numero para decobrir seu sucessor e antecessor '))
+antecessor=valor-1
+sucessor=valor+1
+print(f'Voce digitou o valor {valor}, e seu antecessor é {antecessor}, e seu sucessor é {sucessor}')
