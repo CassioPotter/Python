55a5b6cfb7e9b369af03440dd17b0e00f8bf19fd
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-04 22:51:14 -0300

    Create 016_parte_inteira_de_um_numero.py

---
+++ exercicios/016_parte_inteira_de_um_numero.py
@@ -0,0 +1,7 @@
+from math import trunc
+
+numero=float(input('\nDigite um numero para saber a sua parte inteira '))
+
+numero_inteiro=trunc(numero)
+
+print(f'O numero digitado foi {numero} e a sua parte inteira é {numero_inteiro}')
