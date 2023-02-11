23aebdad8ce8a0d240204a10bedd2b236d2762e1
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-11 17:13:10 -0300

    Create 038_maior_menor_igual.py

---
+++ exercicios/038_maior_menor_igual.py
@@ -0,0 +1,10 @@
+print('\n')
+n1=int(input('Digite o primeiro valor: '))
+n2=int(input('Digite o segundo valor: '))
+
+if n1>n2:
+    print(f'O valor {n1} é maior que {n2}')
+elif n2>n1:
+    print(f'O valor {n2} é maior que {n1}')
+else:
+    print('Os valores são iguais.')
