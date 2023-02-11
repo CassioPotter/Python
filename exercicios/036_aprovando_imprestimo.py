5cb94517f93ea90eef414927f0ba8f36fd1a6b69
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-11 14:54:27 -0300

    Create 036_aprovando_imprestimo.py

---
+++ exercicios/036_aprovando_imprestimo.py
@@ -0,0 +1,17 @@
+print('\n*'*20)
+print('    BANCO POTTER')
+print('*'*20)
+
+casa=float(input('Qual o valor da casa: '))
+salario=float(input('Digite o seu salario: '))
+ano=int(input('Quantos anos de financiamento? '))
+
+prestacao = casa/(ano*12)
+minimo = casa*20/100
+
+if prestacao <= minimo:
+    print(f'Imprestimo concedido, o valor da prestação ficou {prestacao:.2f}R$')
+else:
+    print(('Imprestimo não concedido'))
+
+    
