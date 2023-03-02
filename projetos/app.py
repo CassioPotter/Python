543c91dde2ea36dc6adb97fc3b0bbb16eccb91d3
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-02 11:44:44 -0300

    Create app.py

---
+++ projetos/app.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+menu_inicial = Tk()
+menu_inicial.title('primeiro app')
+
+menu_inicial.geometry('500x500+1+1')
+menu_inicial.resizable(False,False)
+
+
+
+
+
+
+
+menu_inicial.mainloop()
