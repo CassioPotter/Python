c2086782dc5a8ff705dd7b782d4b3069e9d1d93c
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-04 20:51:29 -0300

    Update 08_centraliza.py

--- tkinter/08_centraliza.py
+++ tkinter/08_centraliza.py
@@ -3,13 +3,15 @@ from tkinter import *
 menu_inicial = Tk()
 menu_inicial.title('primeiro app')
 
-menu_inicial.geometry('500x500+100+100')
+menu_inicial.geometry('500x500+200+100')
 menu_inicial.resizable(True, True)
 menu_inicial.minsize(100,100)
 menu_inicial.maxsize(1000,1000)
 
 menu_inicial.iconbitmap('projetos/icon.ico')
 menu_inicial['bg'] = 'blue'
+
+largu
 
 def cmd_Click():
     print('ola mundo')
