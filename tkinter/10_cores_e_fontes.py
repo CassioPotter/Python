395d08d61308793b5866675e2cfab83b1ed6373d
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-04 21:08:04 -0300

    Update 10_cores_e_fontes.py

--- tkinter/10_cores_e_fontes.py
+++ tkinter/10_cores_e_fontes.py
@@ -3,7 +3,7 @@ from tkinter import *
 menu_inicial = Tk()
 menu_inicial.title('primeiro app')
 
-menu_inicial.geometry('500x500+500+100')
+menu_inicial.geometry('500x500')
 menu_inicial.resizable(True, True)
 menu_inicial.minsize(100,100)
 menu_inicial.maxsize(1000,1000)
