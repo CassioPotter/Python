c5f027f272a49671f427dd67356626e65ba7ed22
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-04 21:06:28 -0300

    Create 10_cores_e_fontes.py

---
+++ tkinter/10_cores_e_fontes.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+menu_inicial = Tk()
+menu_inicial.title('primeiro app')
+
+menu_inicial.geometry('500x500+500+100')
+menu_inicial.resizable(True, True)
+menu_inicial.minsize(100,100)
+menu_inicial.maxsize(1000,1000)
+
+menu_inicial.iconbitmap('projetos/icon.ico')
+menu_inicial['bg'] = 'blue'
+
+
+def cmd_Click():
+    print('ola mundo')
+
+def soma():
+    print(f'A soma de 2 + 2 = {2+2}')
+
+cmd = Button(menu_inicial, text='executar', command=cmd_Click)
+cmd.pack()
+
+btn = Button(menu_inicial, text='soma', command=soma)
+btn.pack()
+
+label1 = Label(menu_inicial, text='Esse é o label 1')
+label1.pack()
+
+menu_inicial.mainloop()
