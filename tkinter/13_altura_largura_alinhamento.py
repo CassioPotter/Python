eef78d07e376862692d55fc35036766c064c5bd3
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-04 21:41:25 -0300

    Create 13_altura_largura_alinhamento.py

---
+++ tkinter/13_altura_largura_alinhamento.py
@@ -0,0 +1,82 @@
+from tkinter import *
+
+menu_inicial = Tk()
+menu_inicial.title('primeiro app')
+
+menu_inicial.geometry('500x500')
+menu_inicial.resizable(True, True)
+menu_inicial.minsize(100,100)
+
+menu_inicial.iconbitmap('projetos/icon.ico')
+menu_inicial['bg'] = 'blue'
+
+
+def cmd_Click():
+    print('ola mundo')
+
+def soma():
+    print(f'A soma de 2 + 2 = {2+2}')
+
+cmd = Button(
+    menu_inicial, 
+    text='executar', 
+    command=cmd_Click
+    )
+cmd.pack()
+
+btn = Button(
+    menu_inicial, 
+    text='soma', 
+    command=soma
+    )
+btn.pack()  
+
+label1 = Label(
+                menu_inicial,
+                text='''
+                texto 1
+                texto 2
+                texto 3
+                \n\n\n
+                texto 4''', 
+                bg= 'red',
+                fg='white',
+                font='Arial 20 bold italic',
+                width=100,
+                bd=5,
+                relief='solid'
+                )
+label1.pack()
+
+label2 = Label(
+    menu_inicial,
+    text= 'texto1',
+    font='arial 20',
+    height=2,
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+menu_inicial.mainloop()
