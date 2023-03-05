8a778dbd2979eeb497084e0e7b8ab674b241a578
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-04 21:08:49 -0300

    Update 10_cores_e_fontes.py

--- tkinter/10_cores_e_fontes.py
+++ tkinter/10_cores_e_fontes.py
@@ -6,7 +6,6 @@ menu_inicial.title('primeiro app')
 menu_inicial.geometry('500x500')
 menu_inicial.resizable(True, True)
 menu_inicial.minsize(100,100)
-menu_inicial.maxsize(1000,1000)
 
 menu_inicial.iconbitmap('projetos/icon.ico')
 menu_inicial['bg'] = 'blue'
