37a7c1662e498a6ba973f14622ae70a34b68a365
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-08 23:59:39 -0300

    Update 032_lista.py

--- aulas/032_lista.py
+++ aulas/032_lista.py
@@ -1,4 +1,13 @@
 #Funciona como arrays
 #Não possui tamanho fixo, então pode ser alimentado o quanto quiser 
-#
+#Qualquer tipo de dado
+#Utiliza []
 
+
+print (type([]))
+
+ lista1 = [1, 99, 5, 46, 84, 6, 3]
+lista2 = ['d', '3', 't', '7']
+lista3 = []
+lista4 = [list(range(11))]
+lista5 = list['cassio potter']
