f57cd718331b34cc0fabb0370b6df1a47ebe676f
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-09 21:07:38 -0300

    Update 033_tuplas.py

--- aulas/033_tuplas.py
+++ aulas/033_tuplas.py
@@ -3,8 +3,30 @@ tuplas
 são parecidas com listas, pela diferença de que:
 são representadas por ()
 são imutaveis
+tuplas são definidas pela virgula
 
 """
+tupla1 = (1, 2, 3, 4, 5)
+tupla2 = 1, 2, 3, 4, 5
+tupla3 = (1) #não e uma tupla
+tupla4 = (1,)#é uma tupla
 
 
 print(type(()))
+
+print('\n')
+print(tupla1)
+print(type(tupla1))
+
+print("\n")
+print(tupla2)
+print(type(tupla2))
+
+print('\n')
+print(tupla3)
+print(type(tupla3))
+
+print('\n')
+print(tupla4)
+print(type(tupla4))
+
