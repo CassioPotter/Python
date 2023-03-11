c448ebeeb65989fb189057808985e99bdb133921
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 13:22:16 -0300

    Delete teste_de_velocidade_de_internet.py

--- projetos/teste_de_velocidade_de_internet.py
+++
@@ -1,9 +0,0 @@
-import speedtest #pip install speedtest-cli
-
-test=speedtest.Speedtest()
-
-down=test.download()
-up=test.upload()
-
-print(f'Velocidade download {down}')
-print(f'Velocidade upload {up}')
