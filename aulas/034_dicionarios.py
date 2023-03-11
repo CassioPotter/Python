d7b6339bc95466e785cdb21318ea835aa14d5ca9
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 13:43:21 -0300

    Update 034_dicionarios.py

--- aulas/034_dicionarios.py
+++ aulas/034_dicionarios.py
@@ -4,4 +4,10 @@ são conhecido por mapas
 tipo chave, valor
 mostra chave e o valor
 """
-print({})
+print(type({}))
+print('\n')
+paises = {'br';'Brasil', 'eua':'estados unidos','py':'paraguai'}
+print(type(paises))
+print(paises)
+
+print('\n')
