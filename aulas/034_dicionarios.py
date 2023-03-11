5fc592b9f71dbf0c82ff38406b96729bec6c9af0
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 13:44:04 -0300

    Update 034_dicionarios.py

--- aulas/034_dicionarios.py
+++ aulas/034_dicionarios.py
@@ -6,7 +6,7 @@ mostra chave e o valor
 """
 print(type({}))
 print('\n')
-paises = {'br';'Brasil', 'eua':'estados unidos','py':'paraguai'}
+paises = {'br':'Brasil', 'eua':'estados unidos','py':'paraguai'}
 print(type(paises))
 print(paises)
 
