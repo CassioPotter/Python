a3822f1ad38dba10ea306745552a5c1ba587daff
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-16 20:15:52 -0300

    Update 044_definindo_funçoes.py

--- aulas/044_definindo_funçoes.py
+++ aulas/044_definindo_funçoes.py
@@ -1,2 +1,23 @@
 #funções
 #pequena parte de código que executa tarefa especifica
+
+def lin():
+    print('--'*30)
+
+def men(msg):
+    print('-'*30)
+    print(msg)
+    print('-'*30)
+
+#programa principal
+lin()
+print('cassio potter')
+lin()
+print('karine carmona')
+lin()
+
+print('\n')
+
+men('Cassio Potter')
+men('Karine Carmona')
+
